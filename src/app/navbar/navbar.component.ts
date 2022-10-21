e109d7b12b919f343dd23fe39e880c12997240e9
reinhold <reinhold.beny@ad-alliance.de>
2022-10-21 17:01:30 +0200

    navbar get user data from service and place conditional statements in html sturcture

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../user.service";
+import {User} from "../models/User";
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit(): void {
+    this.user = this.userService.getUserData()
   }
 
 }
