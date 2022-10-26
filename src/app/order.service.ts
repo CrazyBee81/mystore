5c2c76801dc3b34ef211eb0fd54369a5f16a145c
reinhold <reinhold.beny@ad-alliance.de>
2022-10-26 14:50:35 +0200

    oderService createt and method createOrder added

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -7,24 +7,16 @@ import {User} from "./models/User";
   providedIn: 'root'
 })
 export class OrderService {
-  token = ""
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjo3LCJ1c2VybmFtZSI6InRlc3Q0IiwicGFzc3dvcmRfZGlnZXN0IjoiJDJiJDEwJE9iL1kzMWo0cnBvbEhJU2N1S2gueE9FRnpUdUtzcjZ4MGFkSnI4VmFBMzI2N3VSRm85bjJpIn0sImlhdCI6MTY0NjA2MTYzN30.VtaarAXVrViQYciqUmLocet9nqPnrJipqW54P-LOMyY'
-    })
-  };
 
-    constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {
   }
 
   createOrder(user: User): Observable<[]> {
-    this.token = localStorage.getItem('UserToken');
-
+    const token = localStorage.getItem('UserToken');
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        Authorization: `Bearer ${this.token}`
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
       })
     };
 
